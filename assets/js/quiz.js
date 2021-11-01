8aa3495b7dc347c94e6362c16cd263237314f01a
a.breezy <ambrose.wilkins@gmail.com>
2021-11-01 11:58:04 -0400

    added to question logic

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,98 +1,99 @@
-// VARS
-var startDiv = document.querySelector(".start-area");
-var questionContainer = document.querySelector(".question-container");
+// querySelectors of HTML
 var highscoreButtonEl = document.querySelector("#highscoreBtn");
-var timerEl = document.querySelector("#countdownTimer");
+var questionContainer = document.querySelector(".question-container");
 var startButtonEl = document.querySelector("#startBtn");
-// starts timer at 30 seconds
-var startTime = 30;
-// to let function know how many questions are in the array
+var startDiv = document.querySelector(".start-area");
+var timerEl = document.querySelector("#countdownTimer");
+
+// VARS
 var currentQuestionIndex = 0;
 var lastQuestionIndex = -1;
 var randomQuestion = "";
+var score = 0;
+var timer = 30;
 
 // create a list of questions and set as an array with properties: index, question, correct answer
 var questionsArr = [
-  //   {
-  //     question: "What does 'DOM' stand for?",
-  //     answerList: [
-  //       "A. Direct Object Method",
-  //       "B. Direct Occuring Method",
-  //       "C. Document Object Model",
-  //       "D. Document Occuring Measure",
-  //     ],
-  //     correctAnswer: "C. Document Object Model"],
-  //   },
-  //   {
-  //     question: "How does Control Flow Work?",
-  //     answerList: [
-  //       "A. Control Flow creates conditional statements to decide how to proceed",
-  //       "B. Control Flow determins how the browser should interpret the code",
-  //       "C. Control Flow accepts users input and terminates the program",
-  //       "D. Control Flow is like a dam slowing down the browser's speed",
-  //     ],
-  //     correctAnswer: "A. Control Flow creates conditional statements to decide how to proceed",
-  //   },
-  //   {
-  //     question: "What operator do you use to set equality?",
-  //     answerList: ["A. ||", "B. ===", "C. &&", "D. =="],
-  //     correctAnswer: "B. ===",
-  //   },
-  //   {
-  //     question: "How does Pseudocode enhance code?",
-  //     answerList: [
-  //       "A. Put's computer languge into human language",
-  //       "B. Comments code to make it more readable",
-  //       "C. Describes steps for an application to follow",
-  //       "D. All Of The Above",
-  //     ],
-  //     correctAnswer: "D. All Of The Above",
-  //   },
-  //   {
-  //     question: "How do parameters effect functions",
-  //     answerList: [
-  //       "A. They make functions more efficient",
-  //       "B. When set, parameters confine functions to particular tasks",
-  //       "C. They are placeholders for arguments that will be passed into the function",
-  //       "D. Parameters are not related to functions at all",
-  //     ],
-  //     correctAnswer: "C. They are placeholders for arguments that will be passed into the function",
-  //   },
-  //   {
-  //     question: "What is scope",
-  //     answerList: [
-  //       "A. Scope is where functions are accessible from",
-  //       "B. Scope is something that you look through to see things far away",
-  //       "C. Scope is a way to determine a variables functionality",
-  //       "D. Answers A and C",
-  //     ],
-  //     correctAnswer: "D. Answers A and C",
-  //   },
-  //   {
-  //     question: "Fill in the blank: ___(var 1 = 0; i < 5; i++)",
-  //     answerList: ["A. for", "B. if", "C. Switch", "D. var"],
-  //     correctAnswer: "A. for",
-  //   },
-  //   {
-  //     question: "How does 'Math' assist JavaScript Users",
-  //     answerList: [
-  //       "A. 'Math' acts as a calculator",
-  //       "B. 'Math' is a number of built in functions which give the user various functionalities",
-  //       "C. Without 'Math' the user would only be able to use conditional statements",
-  //       "D. 'Math' lets the user conver numbered strings to integer variables",
-  //     ],
-  //     correctAnswer: "B. 'Math' is a number of built in functions which give the user various functionalities",
-  //   },
-  //   {
-  //     question: "How does DOM allow greater functionality",
-  //     answerList: [
-  //       "A. DOM increases memory allocation allowing the browser render images faster",
-  //       "B. DOM let's the user iterate through JavaScript functions",
-  //       "C. DOM is a representation of the html making it manipulatable by JavaScript",
-  //       "D. DOM condenses code into a file that's easier than HTML to read",
-  //     ],
-  //     correctAnswer: "C. DOM is a representation of the html making it manipulatable by JavaScript",
-  //   },
+  // {
+  //   question: "What does 'DOM' stand for?",
+  //   answerList: [
+  //     "A. Direct Object Method",
+  //     "B. Direct Occuring Method",
+  //     "C. Document Object Model",
+  //     "D. Document Occuring Measure",
+  //   ],
+  //   correctAnswer: "C. Document Object Model"],
+  // },
+  // {
+  //   question: "How does Control Flow Work?",
+  //   answerList: [
+  //     "A. Control Flow creates conditional statements to decide how to proceed",
+  //     "B. Control Flow determins how the browser should interpret the code",
+  //     "C. Control Flow accepts users input and terminates the program",
+  //     "D. Control Flow is like a dam slowing down the browser's speed",
+  //   ],
+  //   correctAnswer: "A. Control Flow creates conditional statements to decide how to proceed",
+  // },
+  // {
+  //   question: "What operator do you use to set equality?",
+  //   answerList: ["A. ||", "B. ===", "C. &&", "D. =="],
+  //   correctAnswer: "B. ===",
+  // },
+  // {
+  //   question: "How does Pseudocode enhance code?",
+  //   answerList: [
+  //     "A. Put's computer languge into human language",
+  //     "B. Comments code to make it more readable",
+  //     "C. Describes steps for an application to follow",
+  //     "D. All Of The Above",
+  //   ],
+  //   correctAnswer: "D. All Of The Above",
+  // },
+  // {
+  //   question: "How do parameters effect functions",
+  //   answerList: [
+  //     "A. They make functions more efficient",
+  //     "B. When set, parameters confine functions to particular tasks",
+  //     "C. They are placeholders for arguments that will be passed into the function",
+  //     "D. Parameters are not related to functions at all",
+  //   ],
+  //   correctAnswer: "C. They are placeholders for arguments that will be passed into the function",
+  // },
+  // {
+  //   question: "What is scope",
+  //   answerList: [
+  //     "A. Scope is where functions are accessible from",
+  //     "B. Scope is something that you look through to see things far away",
+  //     "C. Scope is a way to determine a variables functionality",
+  //     "D. Answers A and C",
+  //   ],
+  //   correctAnswer: "D. Answers A and C",
+  // },
+  // {
+  //   question: "Fill in the blank: ___(var 1 = 0; i < 5; i++)",
+  //   answerList: ["A. for", "B. if", "C. Switch", "D. var"],
+  //   correctAnswer: "A. for",
+  // },
+  // {
+  //   question: "How does 'Math' assist JavaScript Users",
+  //   answerList: [
+  //     "A. 'Math' acts as a calculator",
+  //     "B. 'Math' is a number of built in functions which give the user various functionalities",
+  //     "C. Without 'Math' the user would only be able to use conditional statements",
+  //     "D. 'Math' lets the user conver numbered strings to integer variables",
+  //   ],
+  //   correctAnswer: "B. 'Math' is a number of built in functions which give the user various functionalities",
+  // },
+  // {
+  //   question: "How does DOM allow greater functionality",
+  //   answerList: [
+  //     "A. DOM increases memory allocation allowing the browser render images faster",
+  //     "B. DOM let's the user iterate through JavaScript functions",
+  //     "C. DOM is a representation of the html making it manipulatable by JavaScript",
+  //     "D. DOM condenses code into a file that's easier than HTML to read",
+  //   ],
+  //   correctAnswer: "C. DOM is a representation of the html making it manipulatable by JavaScript",
+  // },
   {
     question: "How does localStorage differ from sessionStorage",
     answerList: [
@@ -109,16 +110,15 @@ var questionsArr = [
 // FUNCTS
 // timer starts at 30 sec and goes down. each finished round adds 10 seconds
 var timeLeft = function () {
-  document.getElementById("countdownTimer").textContent = startTime;
-  startTime--;
-  console.log(startTime);
+  document.getElementById("countdownTimer").textContent = timer;
+  timer--;
+  console.log(timer);
 
-  if (startTime === 0) {
-    window.alert("Game Over");
-    clearInterval(startTime);
+  if (timer === 0) {
     gameOver();
+    window.alert("Game Over");
   } else if (currentQuestionIndex === questionsArr.length) {
-    clearTimeout(startTime);
+    clearTimeout(timer);
     document.getElementById("countdownTimer").textContent = 0;
   }
 };
@@ -135,14 +135,33 @@ var showNextQuestion = function () {
 };
 
 var showQuestion = function () {
+  // hide startDiv
   startDiv.classList.add("hide");
 
-  questionContainer.innerText = questionsArr[currentQuestionIndex].question;
-  questionContainer.appendChild("li");
+  // post question as innerText
+  var postQuestion = document.createElement("div");
+  postQuestion.className = "question";
+  postQuestion.textContent = questionsArr[currentQuestionIndex].question;
+  document.currentQuestionIndex.appendChild(postQuestion);
+
+  // post answers as clickable innertext with border
+  var postAnswerList = document.createElement("div");
+  postAnswerList.className = "question-list";
+  postAnswerList.textContent = questionsArr[currentQuestionIndex].answerList[0];
+  currentQuestionIndex.appendChild(postAnswerList);
+
+  //   questionContainer.innerText =
+  //     questionsArr[currentQuestionIndex].answerList[0];
+  //   questionContainer.innerText =
+  //     questionsArr[currentQuestionIndex].answerList[1];
+  //   questionContainer.innerText =
+  //     questionsArr[currentQuestionIndex].answerList[2];
+  //   questionContainer.innerText =
+  //     questionsArr[currentQuestionIndex].answerList[3];
 };
 
 var gameOver = function () {
-  clearInterval(startTime);
+  clearInterval(timer);
   // }
 
   //if timer > 0:
